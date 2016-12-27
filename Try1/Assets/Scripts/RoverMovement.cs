0effe1ca45d707a2a5c152187aec60062dc8f2a7
Simar Singh <simarsher220@gmail.com>
2016-12-27 21:02:23 +0530

    model changed and unity project created

---
+++ Try1/Assets/Scripts/RoverMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverMovement : MonoBehaviour {
+    private Animator anim;
+    float v;
+    float h;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+	// Update is called once per frame
+	void Update () {
+        v = Input.GetAxis("Vertical");
+        h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Forward", v);
+        anim.SetFloat("Backward", h);
+	}
+}
